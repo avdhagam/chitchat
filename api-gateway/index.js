1ab462d82f9f12ea93b198eb3c08e8f406c3bba6
avdhagam <davanisri@gmail.com>
2024-07-03 00:01:32 +0530

    added api-gateway

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -5,7 +5,7 @@ const routes = {
     "/api/auth": "http://localhost:8000/auth",
     "/api/users": "http://localhost:8000/users",
     "/api/msgs": "http://localhost:5000/msgs"
-}
+};
 
 for (const route in routes) {
     const target = routes[route];
